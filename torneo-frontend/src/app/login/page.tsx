08e4909dc6844840d88b3d513480febbbaef64ec
Vijaye Raji <vijaye@statsig.com>
2025-04-02 17:44:28 +0200

    aggiunto frontend Next js

---
+++ torneo-frontend/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Input from "@/components/Input";
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Benvenuto nella Login</h1>
+            <Input></Input>
+        </div>)
+}
---
+++ torneo-frontend/src/components/Input.tsx
@@ -0,0 +1,5 @@
+import TextField from '@mui/material/TextField';
+
+export default function InputComponent() {
+    return <TextField id="filled-basic" label="Filled" variant="filled" />
+}
