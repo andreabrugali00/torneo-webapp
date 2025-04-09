8967d2ab650c2ed45dce6596436b55036fee992a
Vijaye Raji <vijaye@statsig.com>
2025-04-09 17:52:39 +0200

    aggiunto localstorage per il token

--- torneo-frontend/src/components/LoginForm.tsx
+++ torneo-frontend/src/components/LoginForm.tsx
@@ -4,19 +4,24 @@ import InputComponent from "./Input";
 import { UserIcon, LockClosedIcon } from "@heroicons/react/16/solid";
 import { useState, useEffect } from "react";
 import {loginUser} from '../services/authService'
+import { useRouter } from "next/navigation"
 
 export default function LoginForm() {
     const  [email, setEmail] = useState("");
     const  [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
+
+    const router = useRouter()
     const checkCredentials = async () => {
-        console.log(email)
-        console.log(password)
         try {
             const res = await loginUser(email,password)
-            console.log(res)
+            console.log(res.data.token)
+            const token = res.data.token
+            localStorage.setItem("token", token)
+            router.push('./Dashboard')
         } catch (error) {
-            console.log(error)
+            setError("Credenziali non corrette")
         }
     }
     return (
@@ -24,8 +29,9 @@ export default function LoginForm() {
             <InputComponent type="text" text="Email" Icon={UserIcon} onChange={(e)=>setEmail(e.target.value)}></InputComponent>
             <br />
             <InputComponent type="password" text="Password" Icon={LockClosedIcon} onChange={(e) => setPassword(e.target.value)}></InputComponent>
+            {error && <p className="text-red-500">{error}</p>}
             <br />
-            <ButtonComponent text="Accedi" onClick={() => checkCredentials}></ButtonComponent>
+            <ButtonComponent text="Entra" onClick={() => checkCredentials()}></ButtonComponent>
         </div>
     )
 }
--- torneo-frontend/src/services/authService.ts
+++ torneo-frontend/src/services/authService.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 const BASE_URI = process.env.NEXT_PUBLIC_API_URL
-export const loginUser = async (email:string,password:string) =>{
-    try {
-        const res = await axios.post(`${BASE_URI}/auth/login`, {email, password})
-        return res.data
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+export const loginUser = async (email: string, password: string) => {
+    const res = await axios.post(`${BASE_URI}/auth/login`, { email, password })
+    return res.data
 }
